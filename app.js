4a2e38f947f067597a8e83d07b614e8108973465
Afsin Logoglu <muhammedafsin@hotmail.com>
2022-04-28 02:40:33 +0300

    template redesign

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 
 const app = express();
-
-app.get('/', (res, req) => {
-  res.send('Index');
+//template engine 
+app.set("view engine","ejs");
+//middlewares
+app.use(express.static("public"))
+ 
+app.get('/', (req, res) => {
+  res.status(200).render('index',{
+      page_name: "index"
+  });
 });
+app.get('/about', (req, res) => {
+    res.status(200).render('about',{
+        page_name:"about"
+    });
+  });
 
 const port = 3000;
 app.listen(port, () => {
