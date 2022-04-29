670219bc7ff5db5aa2a86054202215c827956235
Afsin Logoglu <muhammedafsin@hotmail.com>
2022-04-29 03:27:01 +0300

    register page added

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -7,4 +7,7 @@ router.route('/').get(pageController.getIndexPage);
 
 router.route('/about').get(pageController.getAboutPage);
 
+router.route('/register').get(pageController.getRegisterPage);
+
+
 module.exports = router;
