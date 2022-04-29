22eeb596a7891bca6f6ecd68fb948ef61045429c
Afsin Logoglu <muhammedafsin@hotmail.com>
2022-04-29 19:22:17 +0300

    login added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
 const pageRoute = require('./routes/pageRoutes');
 const courseRoute = require('./routes/courseRoute');
 const categoryRoute = require('./routes/categoryRoute');
@@ -18,12 +19,25 @@ mongoose
   });
 //template engine
 app.set('view engine', 'ejs');
+
+//global vars
+global.userIN = null;
+
 //middlewares
 app.use(express.static('public'));
 app.use(express.json()) //for parsing app/json
 app.use(express.urlencoded({extended:true}))//for parsing app/x-www-form-urlencoded
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
 
 //routes
+app.use('*',(req,res,next)=>{
+  userIN = req.session.userID;
+  next(); 
+})
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
 app.use('/categories', categoryRoute);
