bc51a6fc01afd4d6ac38611a188909eeab182beb
Afsin Logoglu <muhammedafsin@hotmail.com>
2022-05-09 19:32:54 +0300

    sending email

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,5 @@
+const nodemailer = require('nodemailer');
+
 exports.getIndexPage = (req, res) => {
     res.status(200).render('index',{
         page_name: "index"
@@ -21,4 +23,39 @@ exports.getIndexPage = (req, res) => {
         page_name:"login"
     });
   }
-  
+
+  exports.getContactPage = (req, res) => {
+    res.status(200).render('contact',{
+        page_name:"contact"
+    });
+  }
+
+  exports.sendEmail = async (req, res) => {
+    const outputMessage = `
+    <h1>Mail Detail </h1>
+    <ul>
+    <li>Name: ${req.body.name} </li>
+    <li>Email:${req.body.email} </li>
+    </ul>
+    <h1>Message</h1>
+    <p>${req.body.message}</p>
+    `
+    // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: "afsinlogoglu@gmail.com", // gmail account
+      pass: "123", // gmail password
+    },
+  });
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Smart Edu Contact Form" <afsinlogoglu@gmail.com>', // sender address
+    to: "bar@example.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    html: outputMessage, // html body
+  });
+  res.status(200).redirect('/contact');
+  }
