1a50dbee6d94206aa37b4534df97d93e827275e2
Afsin Logoglu <muhammedafsin@hotmail.com>
2022-05-10 19:19:43 +0300

    admin page added

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -22,6 +22,7 @@ router.route('/login').post(authController.loginUser);
 router.route('/logout').get(authController.logoutUser);
 router.route('/dashboard').get(authMiddleware, authController.getDashboardPage);
 
+router.route('/:id').delete(authController.deleteUser);
 
 
 
